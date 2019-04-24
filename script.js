d6e111d3f97e94d7d86e1cb6b977062a31a70b89
shafali03 <shaf03sm@gmail.com>
2019-04-24 14:09:52 +0100

    update js  structure

--- script.js
+++ script.js
@@ -15,4 +15,40 @@ class UI {
         this.itemList = [];
         this.itemID = 0;
     }
+
 }
+
+function eventListeners() {
+
+    const budgetForm = document.getElementById('budget-form');
+    const expenseForm = document.getElementById('expense-form');
+    const expenseList = document.getElementById('expense-list');
+
+
+    // new instance of UI CLASS
+    const ui = new UI()
+
+
+    // budget form submit
+    budgetForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+    })
+
+    // expense form click
+    expenseForm.addEventListener('click', function (event) {
+        event.preventDefault();
+    })
+
+    // expense click
+    expenseList.addEventListener('click', function () {
+
+    })
+
+
+
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    eventListeners();
+})
