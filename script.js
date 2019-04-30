cfe01805adf2b9c3c077028c57849160540e32e9
shafali03 <shaf03sm@gmail.com>
2019-04-30 19:22:17 +0100

    update App

--- script.js
+++ script.js
@@ -21,11 +21,143 @@ class UI {
         if (value === '' || value < 0) {
             this.budgetFeedback.classList.add('showItem');
             this.budgetFeedback.innerHTML = `<p>Value can not be negative</p>`;
-            // but this method is point to global object
+
+            // but this method is pointing to global object
+            // Fixed error by  const self = this; and change this to self budgetFeedback
+            const self = this;
+            //console.log(this)
+
             setTimeout(function () {
-                this.budgetFeedback.classList.remove('showItem');
-            }, 4000)
+                self.budgetFeedback.classList.remove('showItem');
+            }, 4000);
         }
+        else {
+            this.budgetAmount.textContent = value;
+            this.budgetInput.value = "";
+            this.showBalance();
+        }
+    }
+    // show balance
+    showBalance() {
+        const expense = this.totalExpense();
+        const total = parseInt(this.budgetAmount.textContent) - expense;
+        this.balanceAmount.textContent = total;
+        if (total < 0) {
+            this.balance.classList.remove('showGreen', 'showBlack');
+            this.balance.classList.add('showRed');
+        }
+        else if (total > 0) {
+            this.balance.classList.remove('showRed', 'showBlack');
+            this.balance.classList.add('showGreen');
+        }
+        else if (total === 0) {
+            this.balance.classList.remove('showRed', 'showGreen');
+            this.balance.classList.add('showBlack');
+        }
+    }
+    //submit expense form
+    submitExpenseForm() {
+        const expenseValue = this.expenseInput.value;
+        const amountValue = this.amountInput.value;
+
+        if (expenseValue === '' || amountValue === '' || amountValue < 0) {
+            this.expenseFeedback.classList.add("showItem");
+            this.expenseFeedback.innerHTML = `<p>Values can not be empty or negative</p>`
+            const self = this;
+            setTimeout(function () {
+                self.expenseFeedback.classList.remove('showItem');
+            }, 4000);
+        }
+        else {
+            let amount = parseInt(amountValue);
+            this.expenseInput.value = '';
+            this.amountInput.value = '';
+
+            let expense = {
+                id: this.itemID,
+                title: expenseValue,
+                amount: amount,
+            }
+            this.itemID++;
+            this.itemList.push(expense);
+            this.addExpense(expense)
+            this.showBalance();
+        }
+    }
+    // add expense
+    addExpense(expense) {
+        const div = document.createElement('div');
+        div.classList.add('expense');
+        div.innerHTML = `
+        <div class="expense-item d-flex justify-content-between align-items-baseline">
+
+         <h6 class="expense-title mb-0 text-uppercase list-item">-${expense.title}</h6>
+         <h5 class="expense-amount mb-0 list-item">${expense.amount}</h5>
+
+         <div class="expense-icons list-item">
+
+          <a href="#" class="edit-icon mx-2" data-id="${expense.id}">
+           <i class="fas fa-edit"></i>
+          </a>
+          <a href="#" class="delete-icon" data-id="${expense.id}">
+           <i class="fas fa-trash"></i>
+          </a>
+         </div>
+        </div>
+
+        `;
+        this.expenseList.appendChild(div);
+
+    }
+
+
+    //total expense method
+    totalExpense() {
+        let total = 0;
+        if (this.itemList.length > 0) {
+            total = this.itemList.reduce(function (acc, curr) {
+                // console.log(`total is ${acc} and the current value is ${curr.amount}`);
+                acc += curr.amount;
+                return acc;
+            }, 0)
+        }
+        this.expenseAmount.textContent = total
+        return total;
+    }
+
+    // edit expense
+    editExpense(element) {
+        let id = parseInt(element.dataset.id);
+        let parent = element.parentElement.parentElement.parentElement;
+        //remove from dom
+        this.expenseList.removeChild(parent);
+        // remove from the list
+        let expense = this.itemList.filter(function (item) {
+            return item.id === id;
+        })
+        //show value
+        this.expenseInput.value = expense[0].title;
+        this.amountInput.value = expense[0].amount;
+
+        // remove from list
+        let temporaryList = this.itemList.filter(function (item) {
+            return item.id !== id;
+        })
+        this.itemList = temporaryList;
+        this.showBalance()
+    }
+    // delete expense
+    deleteExpense(element) {
+        let id = parseInt(element.dataset.id);
+        let parent = element.parentElement.parentElement.parentElement;
+        //remove from dom
+        this.expenseList.removeChild(parent);
+        // remove from list
+        let temporaryList = this.itemList.filter(function (item) {
+            return item.id !== id;
+        })
+        this.itemList = temporaryList;
+        this.showBalance()
     }
 }
 
@@ -49,10 +181,18 @@ function eventListeners() {
     // expense form click
     expenseForm.addEventListener('click', function (event) {
         event.preventDefault();
+        ui.submitExpenseForm();
     })
 
     // expense click
-    expenseList.addEventListener('click', function () {
+    expenseList.addEventListener('click', function (event) {
+        if (event.target.parentElement.classList.contains('edit-icon')) {
+            ui.editExpense(event.target.parentElement)
+        }
+        else if (event.target.parentElement.classList.contains('delete-icon')) {
+            ui.deleteExpense(event.target.parentElement)
+
+        }
 
     })
 
